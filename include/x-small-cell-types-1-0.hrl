d7888434f5976d1435a9c3b5a25ad01b1ce74cae
VKI <lib.aca55a@gmail.com>
2015-01-31 20:07:51 +0200

    ignore ebin

--- include/x-small-cell-types-1-0.hrl
+++ include/x-small-cell-types-1-0.hrl
@@ -2,7 +2,9 @@
 -ifndef(_X_TYPES_HRL_).
 -define(_X_TYPES_HRL_, true).
 
--type unsignedInt() :: non_neg_integer().
--type 'IPAddress'() :: integer().
+-include_lib("kernel/include/inet.hrl").
 
--endif(). %% _X_TYPES_HRL_
+-type unsignedInt() :: non_neg_integer().
+-type 'IPAddress'() :: inet:ip_address().
+
+-endif. %% _X_TYPES_HRL_
